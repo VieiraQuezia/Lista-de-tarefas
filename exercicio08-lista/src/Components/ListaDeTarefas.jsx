8d58417bf04354de877d1bd24ada84a00c67ad8b
Quezia Vieira <vieiraquezia24@gmail.com>
2025-02-20 08:36:06 -0300

    alt 2

---
+++ exercicio08-lista/src/Components/ListaDeTarefas.jsx
@@ -0,0 +1,12 @@
+import Tarefa from "./Tarefa";
+import "./ListaDeTarefa.css"
+
+function Lista (props) {
+    return (
+        <>
+         <Tarefa/>
+        </>
+    )
+}
+
+export default Lista
